1fc18734903f216018b80c5824df3a75abb1c95b
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2022-08-30 17:14:43 +0200

    fix: fix-python script

--- util/fix-python.sh
+++ util/fix-python.sh
@@ -2,4 +2,5 @@
 #
 # You need to source config.sh first
 
+WORKSPACE='/city_lmpc_ws'
 export PYTHONPATH=$WORKSPACE/devel/lib/python3/dist-packages:/opt/ros/melodic/lib/python3/dist-packages:$WORKSPACE/devel/lib/python2.7/dist-packages:/opt/ros/melodic/lib/python2.7/dist-packages:
