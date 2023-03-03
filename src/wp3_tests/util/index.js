8da2ee896ace4517a49f62525fa8ec1bba30cdc1
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2023-03-03 14:00:30 +0100

    streamlined index.js for having multiple vehicles

---
+++ src/wp3_tests/util/index.js
@@ -0,0 +1,63 @@
+const { Service, Param } = require("roslib");
+const { ABConnect, ROSPlugin } = require("abconnect-sdk-lite");
+
+const ROS_PLUGIN = new ROSPlugin(9090, true);
+
+let ros = ROS_PLUGIN.ros;
+
+let hostname = require("os").hostname();
+
+let master = "debby";
+let vehicles = [
+    "svea2",
+    "svea5",
+];
+
+let ids = {
+    "debby-(transition)>svea2": 00,
+    "debby-(transition)>svea5": 01,
+    "svea2-(outgoing)>debby": 02,
+    "svea2-(outgoing)>svea5": 04,
+    "svea5-(outgoing)>debby": 05,
+    "svea5-(outgoing)>svea2": 06,
+};
+
+function main() {
+
+    const abconnect = new ABConnect(
+        {host: "wss://intelligent-cluster-manager.herokuapp.com/signal"},
+        {id: hostname},
+        ROS_PLUGIN
+    );
+
+    if (hostname === master) {
+
+        // master -> vehicle
+        vehicles.forEach(name => {
+            abconnect.addDeviceById(name).then(client => {
+                client.addPublisher(ids[`${hostname}-(transition)>${name}`], "transition", "std_msgs/String");
+                client.addSubscriber(ids[`${name}-(incoming)>${hostname}`], "incoming", "wp3_tests/Packet");
+            });
+        });
+
+    } else {
+
+        // vehicle -> master
+        abconnect.addDeviceById(master).then(client => {
+            client.addPublisher(ids[`${hostname}-(outgoing)>${master}`], "outgoing", "wp3_tests/Packet");
+            client.addSubscriber(ids[`${master}-(transition)>${hostname}`], "transition", "std_msgs/String");
+        });
+
+        // vehicle -> vehicle
+        vehicles.forEach(name => {
+            abconnect.addDeviceById(name).then(client => {
+                client.addPublisher(ids[`${hostname}-(outgoing)>${name}`], "outgoing", "wp3_tests/Packet");
+                client.addSubscriber(ids[`${name}-(incoming)>${hostname}`], "incoming", "wp3_tests/Packet");
+            });
+        });
+
+    }
+
+}
+
+main();
