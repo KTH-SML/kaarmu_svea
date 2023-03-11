e62c866db37dbfa9e92f7d913c31a27087f2e6a1
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2023-03-11 19:12:45 +0100

    debugging

--- src/wp3_tests/util/index.js
+++ src/wp3_tests/util/index.js
@@ -1,9 +1,9 @@
-const { Service, Param } = require("roslib");
-const { ABConnect, ROSPlugin } = require("abconnect-sdk-lite");
-
-const ROS_PLUGIN = new ROSPlugin(9090, true);
-
-let ros = ROS_PLUGIN.ros;
+// const { Service, Param } = require("roslib");
+// const { ABConnect, ROSPlugin } = require("abconnect-sdk-lite");
+//
+// const ROS_PLUGIN = new ROSPlugin(9090, true);
+//
+// let ros = ROS_PLUGIN.ros;
 
 let hostname = require("os").hostname();
 
@@ -11,11 +11,11 @@ let n = Number(process.argv[2]);
 let master = process.argv[3];
 let vehicles = process.argv.slice(4, 4+n);
 
-let ids = {};
+let table = {};
 let counter = 0;
 let register = key => {
-    if (ids[key] === undefined)
-        ids[key] = counter++;
+    if (table[key] === undefined)
+        table[key] = counter++;
 };
 
 // Register connections
@@ -29,8 +29,13 @@ vehicles.forEach(self => {
     });
 });
 
+console.log("Master:", master);
+console.log("Vehicles:", vehicles);
+console.log("Connection Table:", table);
+
 function main() {
 
+    const self = hostname;
     const abconnect = new ABConnect(
         {host: "wss://intelligent-cluster-manager.herokuapp.com/signal"},
         {id: hostname},
@@ -38,29 +43,26 @@ function main() {
     );
 
     // Create the connections
-    if (hostname === master) {
+    if (self === master) {
 
-        // master -> vehicle
         vehicles.forEach(name => {
             abconnect.addDeviceById(name).then(client => {
-                client.addPublisher(ids[`${hostname}->${name}`], "transition", "std_msgs/String");
-                client.addSubscriber(ids[`${name}->${hostname}`], "incoming", "wp3_tests/Packet");
+                client.addPublisher(table[`${self}->${name}`], "transition", "std_msgs/String");
+                client.addSubscriber(table[`${name}->${self}`], "incoming", "wp3_tests/Packet");
             });
         });
 
     } else {
 
-        // vehicle -> master
         abconnect.addDeviceById(master).then(client => {
-            client.addPublisher(ids[`${hostname}->${master}`], "outgoing", "wp3_tests/Packet");
-            client.addSubscriber(ids[`${master}->${hostname}`], "transition", "std_msgs/String");
+            client.addPublisher(table[`${self}->${master}`], "outgoing", "wp3_tests/Packet");
+            client.addSubscriber(table[`${master}->${self}`], "transition", "std_msgs/String");
         });
 
-        // vehicle -> vehicle
         vehicles.forEach(name => {
             abconnect.addDeviceById(name).then(client => {
-                client.addPublisher(ids[`${hostname}->${name}`], "outgoing", "wp3_tests/Packet");
-                client.addSubscriber(ids[`${name}->${hostname}`], "incoming", "wp3_tests/Packet");
+                client.addPublisher(table[`${self}->${name}`], "outgoing", "wp3_tests/Packet");
+                client.addSubscriber(table[`${name}->${self}`], "incoming", "wp3_tests/Packet");
             });
         });
 
@@ -68,4 +70,4 @@ function main() {
 
 }
 
-main();
+// main();
